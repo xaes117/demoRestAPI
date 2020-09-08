4e9848d180680af7bee428d19c38f644a31f15ee
Nathan Liu <nathanliu@Nathans-MacBook-Air.local>
2020-09-08 15:44:36 +0100

    Calc longLat coordinate distance

--- src/main/java/com/example/demo/Lib/MathLib.java
+++ src/main/java/com/example/demo/Lib/MathLib.java
@@ -3,7 +3,23 @@ package com.example.demo.Lib;
 public class MathLib {
 
     public static double distance(double lon1, double lat1, double lon2, double lat2) {
-        return 0;
-    }
 
+        if ((lat1 == lat2) && (lon1 == lon2)) {
+            return 0;
+        }
+
+        double theta = lon1 - lon2;
+
+        // Use Haversine formula
+        double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2))
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+
+        dist = Math.acos(dist);
+        dist = Math.toDegrees(dist);
+        dist = dist * 60 * 1.1515 * 0.8684;
+        dist = Math.round(dist);
+
+        return dist;
+
+    }
 }
